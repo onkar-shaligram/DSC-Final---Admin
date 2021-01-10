f9eeaee19cd3a59274ed65bb46af556b78aadbd1
onkar-shaligram <onkarshaligram01@gmail.com>
2021-01-11 01:59:07 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:admin/views/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DSC Events Admin',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
